89972437549accb2ea061bb13d55929266e1d9b0
FonsahPageo <ashprincepageo@gmail.com>
2025-10-07 15:28:29 +0100

    changed alternative API URL to match backend

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-console.log("API URL:", process.env.REACT_APP_API_URL);
-
 root.render(
   <React.StrictMode>
     <App />
